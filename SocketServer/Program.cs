5b700883382af0aa29bb1415a1dc8a06d176c96e
KAWAI~ WOOMINCHAN~ <changwoomin426@gmail.com>
2024-07-07 00:43:01 +0900

    feat : TCP서버 구현

---
+++ SocketServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic TCP Server");
+            StartListening();
+        }
+
+        static string data = null;
+
+        public static void StartListening()
+        {
+            byte[] bytes = new byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Text received : {0}", data);
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.ReadLine();
+        }
+    }
+}
